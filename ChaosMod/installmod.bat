e9662b783ff51a6a04913ba319482646192b2bb8
Slice <51338223+SliceCraft@users.noreply.github.com>
2024-02-20 16:09:31 +0100

    Delete ChaosMod/installmod.bat

--- ChaosMod/installmod.bat
+++
@@ -1,4 +0,0 @@
-move "F:\DevelopmentLethal\ChaosMod\ChaosMod\bin\Debug\ChaosMod.dll" "C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\BepInEx\plugins\ChaosMod.dll"
-cd "C:\Program Files (x86)\Steam\steamapps\common\Lethal Company"
-C:
-"Lethal Company.exe"
